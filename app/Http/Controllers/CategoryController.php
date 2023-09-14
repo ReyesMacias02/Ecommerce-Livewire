15e451af1c51cffd25d3d29a3d7cf9570c6c5b91
jose reyes <jjrm1351828189@gmail.com>
2023-09-13 19:36:40 -0500

    subida

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,6 +8,14 @@ use App\Models\Category;
 
 class CategoryController extends Controller
 {
+ /**
+  * The function "show" in PHP displays a view for a given category.
+  * 
+  * @param Category category The parameter "category" is of type "Category". It is being passed to the
+  * "show" function.
+  * 
+  * @return a view called 'categories.show' and passing the 'category' variable to the view.
+  */
     public function show(Category $category){
         return view('categories.show', compact('category'));
     }
