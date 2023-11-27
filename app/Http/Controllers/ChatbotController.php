9ac5b34f3c70930d81cebcde6fca9a2030140fa7
ReyesMacias02 <jjrm1351828189@gmail.com>
2023-11-27 16:45:14 -0500

    recibir  emit en livewire

--- app/Http/Controllers/ChatbotController.php
+++ app/Http/Controllers/ChatbotController.php
@@ -9,6 +9,7 @@ class ChatbotController extends Controller
 {
     public function handle()
     {
+        dd("llego emit");
         $botman = app('botman');
 
         $botman->hears('Hola', function (BotMan $bot) {
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,40 @@
 <x-app-layout>
 
-  
+    <style>
+        /* Estilos para hacer que el chatbot sea flotante en la esquina superior derecha */
+        #chatbot-container {
+            position: fixed;
+            top: 20px; /* Ajusta la distancia desde la parte superior según tu preferencia */
+            right: 20px; /* Ajusta la distancia desde la derecha según tu preferencia */
+            width: 300px;
+            background-color: #fff;
+            border: 1px solid #ccc;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+            display: flex;
+            flex-direction: column;
+            padding: 10px;
+        }
 
-    <!-- Contenedor del chatbot flotante -->
-    <div id="chatbot-container">
-        <h3 class="text-lg font-semibold text-gray-700 mb-2">Chatbot</h3>
-        <div id="chat-messages" style="max-height: 200px; overflow-y: auto;"></div>
-        <input type="text" id="user-message" class="w-full border rounded-lg p-2 mt-2" placeholder="Escribe un mensaje...">
-        <button id="send-message">Enviar</button>
-    </div>
+        /* Estilos para el botón de enviar */
+        #send-message {
+            background-color: #3490dc;
+            color: #fff;
+            border: none;
+            padding: 8px 16px;
+            border-radius: 4px;
+            cursor: pointer;
+            margin-top: 10px;
+        }
 
+        #send-message:hover {
+            background-color: #2779bd;
+        }
+    </style>
+
+  @livewire('chatbot')
     <div class="container py-8">
-        @foreach ($categories as $category)
-        
+        @foreach ($categories as $category)   
             <section class="mb-6">
                 <div class="flex items-center mb-2">
                     <h1 class="text-lg uppercase font-semibold text-gray-700">
